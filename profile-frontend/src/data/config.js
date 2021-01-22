535654461d235a745345546a91fc29e8c7a533e1
keshav rathi <keshavanilrathi21@gmail.com>
2021-01-23 01:17:11 +0530

    Initial commit

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Keshav Rathi",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Keshav Rathi and I'm a Software Engineer!",
+  githubUrl: "https://github.com/keshavrathi007",
+  linkedinUrl: "https://linkedin.com/ashokkumar1",
+  cvLink: "https://drive.google.com/file/d/1Qu4T0wcThzhpeJusU72XebA5PhjY8c60/view?usp=sharing",
+};
